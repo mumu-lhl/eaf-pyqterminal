71c011e88883d24ffc04bcfbe31745289cbec876
Andy Stewart <lazycat.manatee@gmail.com>
2023-06-03 23:06:52 +0800

    Don't set C-x default, otherwise can't switch to other window when user press `C-x o`, we should use other key to send Ctrl + x.

--- eaf-pyqterminal.el
+++ eaf-pyqterminal.el
@@ -102,7 +102,6 @@ If alpha < 0, don't set alpha for cursor"
     ("C-u" . "eaf-send-key-sequence")
     ("C-v" . "eaf-send-key-sequence")
     ("C-w" . "eaf-send-key-sequence")
-    ("C-x" . "eaf-send-key-sequence")
     ("C-y" . "yank_text")
     ("C-z" . "eaf-send-key-sequence")
     ("M-f" . "eaf-send-key-sequence")
